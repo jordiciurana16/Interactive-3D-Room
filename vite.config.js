bb972dc4218cc960cf8ef6e4f0afb7ce688b7ebf
jordiciurana16 <jciuranas@gmail.com>
2024-09-09 00:33:30 +0200

    music update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Interactive-3D-Room/', // Afegeix això per especificar el camí base correcte
 })
